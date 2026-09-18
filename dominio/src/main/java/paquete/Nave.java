54afc7a38063feef6a592fed2ed55abb9e04ea4f
Junior Peguero <juniorpeguero2013@gmail.com>
2026-09-17 21:33:24 -0300

    Estructura inicial

---
+++ dominio/src/main/java/paquete/Nave.java
@@ -0,0 +1,4 @@
+package paquete;
+
+public abstract class Nave {
+}
---
+++ dominio/src/main/java/paquete/Tripulante.java
@@ -0,0 +1,37 @@
+package paquete;
+
+public abstract class Tripulante {
+    private int id,antiguedad;
+    private String nombre,cargo,planetaOrigen;
+
+
+    public Tripulante(int id, int antiguedad, String nombre, String cargo, String planetaOrigen) {
+        this.id = id;
+        this.antiguedad = antiguedad;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.planetaOrigen = planetaOrigen;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getPlanetaOrigen() {
+        return planetaOrigen;
+    }
+
+}
