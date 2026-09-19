ac66bc0b809e8efbc38f8e17f12e1aaa28de1554
Tomas <tlopez@mdp.edu.ar>
2026-09-19 00:05:26 -0300

    Revert "añado MotorWarp con patron State para cambios de estado y su test."
    This reverts commit bad00af0741b93bc072b411dd40b68d24ec7b57a.

--- dominio/src/main/java/paquete/motorwarp/EstadoWarp.java
+++
@@ -1,9 +0,0 @@
-package paquete.motorwarp;
-
-public interface EstadoWarp {
-        String nombre();
-        void prepararSalto(MotorWarp motor);
-        void iniciarWarp(MotorWarp motor);
-        void finalizarWarp(MotorWarp motor);
-        void finalizarEnfriamiento(MotorWarp motor);
-}
